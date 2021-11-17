5068330b38b0f027d2bb3b8b694f9b02a7a8e76d
Miguel angel Munoz fernandez <mmunoz-f@c2r17s3.42madrid.com>
2021-11-17 21:45:00 +0100

    insert done, works apparently well, delete wip, making final cases

--- utils/red_black_tree.hpp
+++ utils/red_black_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 17:58:46 by mmunoz-f          #+#    #+#             */
-/*   Updated: 2021/11/16 21:07:31 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/11/17 21:44:10 by mmunoz-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,19 @@ namespace ft {
 				return (parent->parent);
 			}
 
+			node	*getSuccesor(node *nill) {
+				node	*s = right;
+				while (s->left != nill)
+					s = s->left;
+				return (s);
+			}
+
+			node	*getSibling() {
+				if (this == parent->right)
+					return (parent->left);
+				return (parent->right);
+			}
+
 			T&		operator*() { return (content); }
 			T*		operator->() { return (&content); }
 	};
@@ -86,7 +99,7 @@ namespace ft {
 	template<class T, class Compare, class Allocator>
 	class	tree {
 		public:
-			typedef T														value_type;
+			typedef T													value_type;
 
 			typedef node<T>												node;
 			typedef typename Allocator::template rebind<node>::other	Tree_allocator;
@@ -98,10 +111,10 @@ namespace ft {
 		private:
 			void	leftRotate(node *x) {
 				node	*y = x->right;
-				if (y->left != _nill) {
+				x->right = y->left;
+				if (y->left != _nill)
 					y->left->parent = x;
-					x->right = y->left;
-				}
+				y->parent = x->parent;
 				if (x->parent == _nill)
 					_root = y;
 				else if (x == x->parent->left)
@@ -114,10 +127,10 @@ namespace ft {
 
 			void	rightRotate(node *y) {
 				node	*x = y->left;
-				if (x->right != _nill) {
+				y->left = x->right;
+				if (x->right != _nill)
 					x->right->parent = y;
-					y->left = x->right;
-				}
+				x->parent = y->parent;
 				if (y->parent == _nill)
 					_root = x;
 				else if (y == y->parent->right)
@@ -140,45 +153,124 @@ namespace ft {
 				leftRotate(z);
 			}
 
+			static void	reColorInsert(node *n) {
+				n->getGrandParent()->right->color = M_BLACK;
+				n->getGrandParent()->left->color = M_BLACK;
+				n->getGrandParent()->color = M_RED;
+			}
+
+			void	leftCase(node *n) {
+				if (n->getGrandParent()->right->color == M_RED) {
+					reColorInsert(n);
+					n = n->getGrandParent();
+				}
+				else {
+					if (n == n->parent->right) { // Case 2
+						n = n->parent;
+						leftRotate(n);
+					}
+					n->parent->color = M_BLACK;
+					n->getGrandParent()->color = M_RED;
+					rightRotate(n->getGrandParent());
+				}
+			}
+
+			void	rightCase(node *n) {
+				if (n->getGrandParent()->left->color == M_RED) {
+					reColorInsert(n);
+					n = n->getGrandParent();
+				}
+				else {
+					if (n == n->parent->left) {
+						n = n->parent;
+						rightRotate(n);
+					}
+					n->parent->color = M_BLACK;
+					n->getGrandParent()->color = M_RED;
+					leftRotate(n->getGrandParent());
+				}
+			}
+
 			void	fixInsert(node *newNode) {
-				while (newNode->parent->color != M_RED) {
-					std::cout << "hola" << std::endl;
-					if (newNode->getGrandParent()->left == newNode->parent) { // Case 1
-						if (newNode->getGrandParent()->right->color == M_RED) {
-							newNode->getGrandParent()->right = M_BLACK;
-							newNode->getGrandParent()->left = M_BLACK;
-							newNode->getGrandParent()->color = M_RED;
-							newNode = newNode->getGrandParent();
-						}
-						else if (newNode == newNode->parent->right) { // Case 2
-							newNode = newNode->parent;
-							leftRotate(newNode);
-						}
-						else { // Case 3
-							newNode->parent->color = M_BLACK;
-							newNode->getGrandParent()->color = M_RED;
-							rightRotate(newNode->getGrandParent());
-						}
-					}
+				while (newNode->parent->color == M_RED) {
+					if (newNode->getGrandParent()->left == newNode->parent) // Case 1
+						leftCase(newNode);
 					else {
-						if (newNode->getGrandParent()->left->color == M_RED) {
-							newNode->getGrandParent()->left->color = M_BLACK;
-							newNode->getGrandParent()->right->color = M_BLACK;
-							newNode->getGrandParent()->color = M_RED;
-							newNode = newNode->getGrandParent();
-						}
-						else {
-							if (newNode == newNode->parent->left) {
-								newNode = newNode->parent;
-								rightRotate(newNode);
-							}
-							newNode->parent->color = M_BLACK;
-							newNode->getGrandParent()->color = M_RED;
-							leftRotate(newNode->getGrandParent());
-						}
+						rightCase(newNode);
 					}
+					if (newNode == _root)
+						break;
 				}
 				_root->color = M_BLACK;
+			}
+
+			node	*replaceNill(node *n) {
+				node	*x;
+				x = (n->left == _nill ? n->right : n->left);
+				if (n->parent->left == n)
+					n->parent->left = x;
+				else
+					n->parent->right = x;
+				return (x);
+			}
+
+			static bool	deleteCase0(node &*x) {
+				x->color = M_BLACK;
+				return (false);
+			}
+
+			static bool	deleteCase1(node &*x, node &*w) {
+				w->color = M_BLACK;
+				x->parent->color = M_RED;
+				if (x == x->parent->left)
+					leftRotate(x->parent);
+				else
+					rightRotate(x->parent);
+				if (x == x->parent->left)
+					w = x->parent->right;
+				else
+					w = x->parent->left;
+				return (true);
+			}
+
+			static bool	deleteCase2(node &*x, node &*w) {
+				w->color = M_RED;
+				x = x->parent;
+				if (x->color == M_RED) {
+					x->color = M_BLACK;
+					return (false);
+				}
+				w = x->getSibling();
+				return (true);
+			}
+
+			static bool	deleteCase3(node &*x, node &*w) {
+				x->parent->left == x ? w->left->color = M_BLACK : w->right->color = M_BLACK;
+				w->color = M_RED;
+
+				// TODO estas por aqui
+
+			}
+
+			static bool	reColorDelete(node &*x, node &*w) {
+				if (x->color == M_RED)
+					return (deleteCase0(x));
+				else if (x->color == M_BLACK && w->color == M_RED)
+					return (deleteCase1(x, w));
+				else if (x->color == M_BLACK && w->color == M_BLACK && w->right->color == M_BLACK && w->left->color == M_BLACK)
+					return (deleteCase2(x, w));
+				else if (x->color == M_BLACK && w->color == M_BLACK
+					&& ((x->parent->left == x && w->left->color == M_RED && w->right->color == M_BLACK)
+						|| (x->parent->right == x && w->right->color == M_RED && w->left->color == M_BLACK)))
+					return (deleteCase3(x, w));
+				return (true);
+			}
+
+			void	resetNill() {
+				_nill->color = M_BLACK;
+				_nill->parent = _nill;
+				_nill->right = _nill;
+				_nill->left = _nill;
 			}
 
 		public:
@@ -186,6 +278,7 @@ namespace ft {
 			/* CONSTRUCTORS */
 
 			tree(const Compare &comp, const Tree_allocator &alloc) : _comp(comp), _alloc(alloc), _nill(_alloc.allocate(1)), _root(_nill) {
+				_nill->color = M_BLACK;
 				_nill->parent = _nill;
 				_nill->left = _nill;
 				_nill->right = _nill;
@@ -232,14 +325,14 @@ namespace ft {
 				}
 				node	*i = _root;
 				node	*p;
-				for (; i != _nill;) {
+				while (i != _nill) {
 					p = i;
 					if (_comp(*(*i), *(*newNode)))
 						i = i->right;
 					else
 						i = i->left;
 				}
-				newNode->parent = i->parent;
+				newNode->parent = p;
 				newNode->color = M_RED;
 				_comp(*(*p), *(*newNode)) ? p->right = newNode : p->left = newNode;
 				fixInsert(newNode);
@@ -248,9 +341,27 @@ namespace ft {
 
 			/* DELETE */
 
-			// void	deleteNode(node	*node) {
-			// 	bool	originalColor = node->color;
-			// }
+			void	deleteNode(node	*n) {
+				bool	originalColor = n->color;
+				node	*x;
+				if (n->left == _nill || n->right == _nill)
+					x = replaceNill(node);
+				else {
+					x = n->getSuccesor(_nill);
+					x->parent->left = _nill;
+				}
+				if (n->color == M_RED && (x == _nill || x->color == M_RED))
+					return ;
+				else if (n->color == M_RED && x->color == M_BLACK)
+					x->color = M_RED;
+				else if (n->color == M_BLACK && x->color == M_RED) {
+					x->color = M_BLACK;
+					return ;
+				}
+				node	*w = x->getSibling();
+				while (reColorDelete(x, w));
+				resetNill();
+			}
 			/* --------- */
 
 			void print(const std::string& prefix = std::string(), const node* node = NULL, bool isLeft = false) const {
